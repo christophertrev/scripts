c2d0e93db967ae03511326c2839777a93f79a8a8
christophertrev <christophertrev@localhost.localdomain>
2014-04-27 21:07:37 -0700

    added python-setup.sh

---
+++ .profile
@@ -0,0 +1,3 @@
+export WORKON_HOME=/home/christophertrev/virtualenv
+export PROJET_HOME=/home/christophertrev/Devel
+source /usr/local/bin/virtualenvwrapper.sh
---
+++ python-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+sudo apt-get install python-pip 
+
+
+sudo pip install virtualenvwrapper
+
+
+echo "export WORKON_HOME=$HOME/virtualenv
+export PROJET_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
+
+
+
